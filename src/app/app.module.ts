aa24060ba98aeb133f892be79e73f420892b6917
rafael <flns_rafa@hotmail.com>
2020-01-12 23:05:46 +0000

    Trabalhando com routes no angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-
+import {Routes, RouterModule} from '@angular/router';
 import {registerLocaleData} from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+
+const routes: Routes = [
+  {path: 'lancamentos', component: LancamentosPesquisaComponent},
+  {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+  {path: 'pessoas', component: PessoasPesquisaComponent}
+];
 
 registerLocaleData(localePt);
 @NgModule({
@@ -17,6 +26,7 @@ registerLocaleData(localePt);
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    RouterModule.forRoot(routes),
 
     LancamentosModule,
     PessoasModule,
